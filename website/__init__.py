f9d0ecfbe86eac2b17f197e947a8df29bd035bd1
許家豪 <steven@xujiahaodeMacBook-Pro.local>
2023-03-09 14:04:16 +0800

    Flask app

---
+++ website/__init__.py
@@ -0,0 +1,38 @@
+import os
+from os import path
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, urlprefix="/")
+    app.register_blueprint(auth, urlprefix="/")
+
+    from .models import Note, User
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
